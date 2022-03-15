openharmony/third_party_nghttp2

commit 929dea9019d56f481d83f9b3e58d11ff28277b07

nghttpx: Randomize backend address round robin order per thread

src/shrpx_worker.cc
@@ -304,6 +304,9 @@ void Worker::replace_downstream_config(
       shared_addr->http1_pri.weight = num_http1;
       shared_addr->http2_pri.weight = num_http2;
 
+      std::shuffle(std::begin(shared_addr->addrs), std::end(shared_addr->addrs),
+                   randgen_);
+
       if (shared_addr->affinity.type != SessionAffinity::NONE) {
         for (auto &addr : shared_addr->addrs) {
           addr.dconn_pool = std::make_unique<DownstreamConnectionPool>();

commit ad4580ecbefe43d03092ba597312bec5b48a141f

nghttpx: Fix broken session affinity
Session affinity has been broken since
929dea9019d56f481d83f9b3e58d11ff28277b07.  This commit reverts the
relevant part of the commit to fix the session affinity.

src/shrpx_worker.cc
@@ -333,9 +333,6 @@ void Worker::replace_downstream_config(
     auto it = addr_groups_indexer.find(dkey);
 
     if (it == std::end(addr_groups_indexer)) {
-      std::shuffle(std::begin(shared_addr->addrs), std::end(shared_addr->addrs),
-                   randgen_);
-
       auto shared_addr_ptr = shared_addr.get();
 
       for (auto &addr : shared_addr->addrs) {